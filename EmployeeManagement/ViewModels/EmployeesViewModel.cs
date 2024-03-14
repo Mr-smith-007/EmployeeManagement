f2edd190eea5869b11259ba7191247a8f020acd8
Mr-Smith-007 <mr_smith_007@mail.ru>
2024-03-14 14:29:38 +0300

    Unit_4

---
+++ EmployeeManagement/ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        private EmployeeRepository _repos { get; }
+        public EmployeesViewModel() 
+        {
+            _repos = new EmployeeRepository();
+        }
+
+        public ObservableCollection<Employee> Employees
+        {
+            get
+            {
+                return new ObservableCollection<Employee>
+                    (this._repos.GetAll());
+            }
+        }
+    }
+}
